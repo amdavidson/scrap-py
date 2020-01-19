963379c5b84697bd51f802ac8966cfb8f9c3161d
Jeremy L <JeremyLaBarge@gmail.com>
2020-01-19 07:52:38 -0800

    fix bug and white space changes

--- analyze.py
+++ analyze.py
@@ -3,33 +3,15 @@
 import pandas as pd
 
 
-
 def get_resources(string, column, mem):
     try:
         l = mem[string]
     except KeyError:
         l = lf[lf[column].str.match(string, na=False)]['RESOURCE_ID'].unique()
         l.sort()
-        pn_memory[string] = l
+        mem[string] = l
     return l
 
-def get_resources_pn(pn, pn_memory):
-    try:
-        l = pn_memory[pn[:6]]
-    except KeyError:
-        l = lf[lf['PART_ID'].str.match(pn[:6], na=False)]['RESOURCE_ID'].unique()
-        l.sort()
-        pn_memory[pn[:6]] = l
-    return l
-
-def get_resources_wo(wo, wo_memory):
-    try:
-        l = wo_memory[wo]
-    except KeyError:
-        l = lf[lf['WORKORDER_BASE_ID'].str.match(wo,na=False)]['RESOURCE_ID'].unique()
-        l.sort()
-        wo_memory[wo] = l
-    return l
 
 def str_arr(arr):
     return ' '.join(str(s) for s in arr)
@@ -40,15 +22,16 @@ if __name__ == "__main__":
     sf = pd.read_csv("scrap-dummy.csv", encoding="ISO-8859-1", dtype=str)
     lf = pd.read_csv("lt-dummy.csv", encoding = "ISO-8859-1", dtype=str)
 
-    lf['sub_PART_ID']=lf['PART_ID'].str.slice(0,5)
+    lf['sub_PART_ID'] = lf['PART_ID'].str.slice(0, 5)
     pn_memory = lf.sort_values(['RESOURCE_ID']).groupby(
-            ['sub_PART_ID'])['RESOURCE_ID'].unique().apply(list).to_dict()
+        ['sub_PART_ID'])['RESOURCE_ID'].unique().apply(list).to_dict()
     sf['ResourceList_byPN'] = sf.apply(lambda r:
-            str_arr(get_resources(r.PART[:6], 'PART_ID', pn_memory)), axis = 1)
+            str_arr(get_resources(r.PART[:6], 'PART_ID', pn_memory)), axis=1)
 
     wo_memory = lf.sort_values(['RESOURCE_ID']).groupby(
             ['WORKORDER_BASE_ID'])['RESOURCE_ID'].unique().apply(list).to_dict()
     sf['ResourceList_byWO'] = sf.apply(lambda r:
-            str_arr(get_resources(r.WO, 'WORKORDER_BASE_ID', wo_memory)), axis = 1)
+            str_arr(get_resources(r.WO, 'WORKORDER_BASE_ID', wo_memory)), axis=1)
+
     print(sf.to_csv())
 
